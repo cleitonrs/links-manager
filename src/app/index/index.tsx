86a358d891df520c7bf8ad2dc53f8b3efcc4fe91
cleitonrs <rscleitonrs@gmail.com>
2025-04-19 18:06:19 -0300

    added colors styles

---
+++ src/app/index/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+import { styles } from "./styles"
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello React Native!</Text>
+      <Text style={styles.title}>Meu app</Text>
+    </View>
+)
+}
