372ca4483d05078a51c36edf890a815b553c6c29
cleitonrs <rscleitonrs@gmail.com>
2025-04-21 18:39:47 -0300

    added async storage

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,4 +1,5 @@
-import { View, Text, TouchableOpacity } from "react-native"
+import { useState } from "react"
+import { View, Text, TouchableOpacity, Alert } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 import { router } from "expo-router"
 
@@ -6,9 +7,24 @@ import { styles } from "./styles"
 import { colors } from "@/styles/colors"
 
 import { Input } from "@/components/input"
+import { Button } from "@/components/button"
 import { Categories } from "@/components/categories"
 
 export default function Add() {
+  const [category, setCategory] = useState("")
+  const [name, setName] = useState("")
+  const [url, setUrl] = useState("")
+
+  function handleAdd() {
+    if (!category) return Alert.alert("Categoria", "Selecione a categoria")
+    
+    if (!name.trim()) return Alert.alert("Nome", "Informe o nome")
+
+    if (!url.trim()) return Alert.alert("URL", "Informe a URL")
+
+    console.log({category, name, url})
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -21,11 +37,12 @@ export default function Add() {
         </Text>
       </View>
       <Text style={styles.label}>Selecione uma categoria</Text>
-      <Categories />
+      <Categories onChange={setCategory} selected={category} />
 
       <View style={styles.form}>
-        <Input placeholder="nome" />
-        <Input placeholder="Url" />
+        <Input placeholder="nome" onChangeText={setName} autoCorrect={false} />
+        <Input placeholder="URL" onChangeText={setUrl} autoCorrect={false} />
+        <Button title="Adicionar" onPress={handleAdd} />
       </View>
     </View>
   )
--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -4,12 +4,16 @@ import  { router } from "expo-router"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
+import { categories } from "@/utils/categories"
 
 import { Link } from "@/components/link"
 import { Option } from "@/components/option"
 import { Categories } from "@/components/categories"
+import { useState } from "react"
 
 export default function Index() {
+  const [category, setCategory] = useState(categories[0].name)
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -20,7 +24,7 @@ export default function Index() {
         </TouchableOpacity>
       </View>
 
-      <Categories />
+      <Categories onChange={setCategory} selected={category} />
 
       <FlatList
         data={["1", "2", "3", "4", "5"]}
